8cc822e131eda1610e262e8e4258715badc4fff0
Eric Milaneze <eric.milaneze@klir.io>
2023-11-30 19:36:02 -0300

    adding DoublyLinkedList.SwapFirstAndLast()

--- doublylinkedlist/swap-first-and-last.go
+++ doublylinkedlist/swap-first-and-last.go
@@ -1,5 +1,11 @@
 package doublylinkedlist
 
 func (dll *DoublyLinkedList) SwapFirstAndLast() {
+	if dll.Length <= 1 {
+		return
+	}
 
+	temp := dll.Head.Value
+	dll.Head.Value = dll.Tail.Value
+	dll.Tail.Value = temp
 }
