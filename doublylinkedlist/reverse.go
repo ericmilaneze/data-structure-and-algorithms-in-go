2d751c8813e4a8ea5ead7a693df2d2d6930b8428
Eric Milaneze <eric.milaneze@klir.io>
2023-11-30 19:57:40 -0300

    adding DoublyLinkedList.Reverse()

--- doublylinkedlist/reverse.go
+++ doublylinkedlist/reverse.go
@@ -1,5 +1,19 @@
 package doublylinkedlist
 
 func (dll *DoublyLinkedList) Reverse() {
+	if dll.Length <= 1 {
+		return
+	}
 
+	fromHead := dll.Head
+	fromTail := dll.Tail
+
+	for i := 0; i < dll.Length/2; i++ {
+		temp := fromHead.Value
+		fromHead.Value = fromTail.Value
+		fromTail.Value = temp
+
+		fromHead = fromHead.Next
+		fromTail = fromTail.Previous
+	}
 }
