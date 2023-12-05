54add761b8896363475b4c9646a597a9aa508452
Eric Milaneze <eric.milaneze@klir.io>
2023-12-05 08:08:13 -0300

    adding DoublyLinkedList.IsPalindrome()

--- doublylinkedlist/palindrome-checker.go
+++ doublylinkedlist/palindrome-checker.go
@@ -1,5 +1,21 @@
 package doublylinkedlist
 
 func (dll DoublyLinkedList) IsPalindrome() bool {
-	return false
+	if dll.Length <= 1 {
+		return true
+	}
+
+	nodeFromHead := dll.Head
+	nodeFromTail := dll.Tail
+
+	for i := 0; i < dll.Length/2; i++ {
+		if nodeFromHead.Value != nodeFromTail.Value {
+			return false
+		}
+
+		nodeFromHead = nodeFromHead.Next
+		nodeFromTail = nodeFromTail.Previous
+	}
+
+	return true
 }
